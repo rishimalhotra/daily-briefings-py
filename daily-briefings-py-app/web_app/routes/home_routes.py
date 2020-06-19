5c10101a8b6ba1ff968551e832f0fdd4edff9a53
Rishi Malhotra <rishirmalhotra@gmail.com>
2020-06-18 21:31:38 -0400

    class with html stuff through checkpoint 3

---
+++ daily-briefings-py-app/web_app/routes/home_routes.py
@@ -0,0 +1,22 @@
+# web_app/routes/home_routes.py
+
+from flask import Blueprint, render_template
+
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route("/")
+def index():
+    print("VISITED THE HOME PAGE")
+    #return "Welcome Home (TODO)"
+    return render_template("home.html")
+
+@home_routes.route("/about")
+def about():
+    print("VISITED THE ABOUT PAGE")
+    #return "About Me (TODO)"
+    return render_template("about.html")
+
+@home_routes.route("/users/new")
+def register():
+    print("VISITED THE REGISTRATION PAGE")
+    return "Sign Up for our Product! (TODO)" # we'll make an HTML template for this later!
